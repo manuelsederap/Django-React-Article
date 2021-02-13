b96fe94a4202ce2ccd5b51c1e74069aef2f45655
manuel_paredes <manuelparedesjr0693@gmail.com>
2021-02-14 00:10:31 +0800

    Add CRUD for articles functional based

--- APIProject/api/views.py
+++ APIProject/api/views.py
@@ -1,5 +1,57 @@
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
+from .models import Article
+from .serializers import ArticleSerializer
 
 
-def index(request):
-    return HttpResponse("Hello, World")
+@csrf_exempt
+def article_list(request):
+    """Create data or Get all data of article"""
+
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def article_details(request, pk):
+    """Update/Read/Delete the article"""
+
+    try:
+        article = Article.objects.get(pk=pk)
+    
+    except Article.DoesNotExist:
+        response = ({'message': 'No data found...'})
+        return JsonResponse(response, status=404)
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(article, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        article.delete()
+        response = ({'message': 'Sucessfully Deleted...'})
+        return JsonResponse(response, status=204)
+
+
