1c0cb388221815264309e03ab5ef65f5f29bb531
manuel_paredes <manuelparedesjr0693@gmail.com>
2021-02-14 12:07:12 +0800

    Add viewset view

--- APIProject/api/views.py
+++ APIProject/api/views.py
@@ -3,9 +3,48 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics, mixins
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
 
 from .models import Article
 from .serializers import ArticleSerializer
+
+
+class ArticleViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for listing or creating Article.
+    """
+    def list(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        queryset = Article.objects.all()
+        article = get_object_or_404(queryset, pk=pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, pk=None):
+        article = Article.objects.get(pk=pk)
+        serializer = ArticleSerializer(article, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        article = Article.objects.get(pk=pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class ArticleList(generics.GenericAPIView,
