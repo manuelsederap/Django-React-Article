fe509300851fbcd68994478e65e838b39ee5795b
manuel_paredes <manuelparedesjr0693@gmail.com>
2021-02-14 10:59:06 +0800

    Add generic and mixin views

--- APIProject/api/views.py
+++ APIProject/api/views.py
@@ -2,58 +2,90 @@ from django.shortcuts import render, HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics, mixins
 
 from .models import Article
 from .serializers import ArticleSerializer
 
 
-class ArticleList(APIView):
-    """List all Article or Create a new article"""
+class ArticleList(generics.GenericAPIView,
+                    mixins.ListModelMixin,
+                    mixins.CreateModelMixin):
+    """Get and Create data using Generic and Mixin"""
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
 
-    def get(self, request, format=None):
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        return Response(serializer.data)
+    def get(self, request):
+        return self.list(request)
 
-    def post(self, request, format=None):
-        serializer = ArticleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        return self.create(request)
+
+# class ArticleList(APIView):
+#     """List all Article or Create a new article using class based view"""
+
+#     def get(self, request, format=None):
+#         articles = Article.objects.all()
+#         serializer = ArticleSerializer(articles, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request, format=None):
+#         serializer = ArticleSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class ArticleDetails(APIView):
-    """Retrived, Update, Delete article instance"""
+class ArticleDetails(generics.GenericAPIView,
+                        mixins.RetrieveModelMixin,
+                        mixins.UpdateModelMixin,
+                        mixins.DestroyModelMixin):
+    """Retrive, Update, Delete data using Generic and Mixin"""
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    lookup_field = 'id'
 
-    def get(self, request, id, format=None):
-        try:
-            article = Article.objects.get(id=id)
-            serializer = ArticleSerializer(article)
-            return Response(serializer.data)
-        except Article.DoesNotExist:
-            response = ({'message': 'No data found...'})
-            return Response(response, status=status.HTTP_404_NOT_FOUND)
+    def get(self, request, id):
+        return self.retrieve(request, id=id)
 
-    def put(self, request, id, format=None):
-        article = Article.objects.get(id=id)
-        serializer = ArticleSerializer(article, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, id):
+        return self.update(request, id=id)
 
-    def delete(self, request, id, format=None):
-        article = Article.objects.get(id=id)
-        article.delete()
-        response = ({'message': 'Sucessfully Deleted...'})
-        return Response(response, status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, id):
+        return self.destroy(request, id=id)
+
+                
+# class ArticleDetails(APIView):
+#     """Retrived, Update, Delete article instance using class based view"""
+
+#     def get(self, request, id, format=None):
+#         try:
+#             article = Article.objects.get(id=id)
+#             serializer = ArticleSerializer(article)
+#             return Response(serializer.data)
+#         except Article.DoesNotExist:
+#             response = ({'message': 'No data found...'})
+#             return Response(response, status=status.HTTP_404_NOT_FOUND)
+
+#     def put(self, request, id, format=None):
+#         article = Article.objects.get(id=id)
+#         serializer = ArticleSerializer(article, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, id, format=None):
+#         article = Article.objects.get(id=id)
+#         article.delete()
+#         response = ({'message': 'Sucessfully Deleted...'})
+#         return Response(response, status=status.HTTP_204_NO_CONTENT)
         
 
 # @api_view(['GET', 'POST'])
 # def article_list(request):
-#     """Create data or Get all data of article"""
+#     """Create data or Get all data of article using function based"""
 
 #     if request.method == 'GET':
 #         articles = Article.objects.all()
@@ -69,7 +101,7 @@ class ArticleDetails(APIView):
 
 # @api_view(['PUT', 'GET', 'DELETE'])
 # def article_details(request, pk):
-#     """Update/Read/Delete the article"""
+#     """Update/Read/Delete the article using function based"""
 
 #     try:
 #         article = Article.objects.get(pk=pk)
